229c7da580b4c0c512eea8c63845b55bcada3e6f
Panha <thopanha123@gmail.com>
2025-05-04 13:54:17 +0700

    design UI forget password and setup firebase package firebase_core and add google-services.json

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,7 +3,12 @@ allprojects {
         google()
         mavenCentral()
     }
+
 }
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
