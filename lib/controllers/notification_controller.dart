3d655516ca57b482110784057f8c1526653a7153
Panha <thopanha123@gmail.com>
2025-05-10 14:36:33 +0700

    Notification firebase CLI

---
+++ lib/controllers/notification_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationController extends GetxController {
+  final _notifications = <RemoteMessage>[].obs;
+  final _unreadCount = 0.obs;
+
+  List<RemoteMessage> get notifications => _notifications.toList();
+  int get unreadCount => _unreadCount.value;
+
+  void addNotification(RemoteMessage message) {
+    _notifications.insert(0, message);
+    _unreadCount.value++;
+  }
+
+  void markAsRead() {
+    _unreadCount.value = 0;
+  }
+
+  void clearAll() {
+    _notifications.clear();
+    _unreadCount.value = 0;
+  }
+}
